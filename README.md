d809ac3e8a94d2d2b1ea51659a80f7c038d819c9
JosaelPrz <josael.prz@gmail.com>
2024-04-26 09:57:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# OS Keymaps
+
+- Search: Meta
+- Open explorer: Meta-e
+- Open browser: Meta-b
+- Open settings: Meta-s
+- Open terminal: Meta-t
+- Open notifications: Meta-n
+- Maximize window: Meta-k
+- Hide window: Meta-j
+- Attach window to left: Meta-h
+- Attach window to right: Meta-l
+- Lock screen: Meta-0
+- Logout: Meta-o
+- Change language: Meta-Space
+- Change tab: Alt-Tab
+
+# Windows
+
+- Open window: Control-n
+- Close window: Control-Shift-w
+- Open tab: Control-t
+- Close tab: Control-w
+- Open panel right (Split vertically): Control-v, Leader-v
+- Open panel down (Split horizontally): Control-b, Leader-b
+- Close panel: Control-c, Leader-c
+- Top panel: Control-k
+- Bottom panel: Control-j
+- Left panel: Control-h
+- Right panel: Control-l
+
+## Globals
+
+- Neovim Leader: Space
+- Neovim Escape: jj
+- OS Navigation: Meta
+- Panel Navigation: Ctrl
+- Tab Navigation: Ctrl-Alt
+- Menu Navigation: Alt
+- Cursor Navigation: HJKL
+- Cursor Actions: Shift-HJKL
+- Open Completion Menu: Ctrl-Space
+- Accept Completion: Enter
+- Accept Inline Completion: Tab
---
+++ setup.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+# NOTE: Update
+sudo apt update
+
+# NOTE: Install Curl
+sudo apt install -y curl
+
+# NOTE: Install Wget
+sudo apt install -y wget
+
+# NOTE: Install Git CLI
+sudo apt install -y git
+
+# NOTE: Install GitHub CLI 
+sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install -y gh
+
+# NOTE: Install Mongosh CLI 
+sudo curl -o mongosh.deb https://downloads.mongodb.com/compass/mongodb-mongosh_2.2.5_amd64.deb
+sudo apt install -y ./mongosh.deb
+sudo rm ./mongosh.deb
+
+# NOTE: Install Atlas CLI 
+sudo curl -o atlas.deb https://fastdl.mongodb.org/mongocli/mongodb-atlas-cli_1.20.0_linux_x86_64.deb
+sudo apt install -y ./atlas.deb
+sudo rm ./atlas.deb
+
+# NOTE: Install Neovim
+sudo apt install -y neovim
+
+# NOTE: Install Zsh
+sudo apt install -y zsh
+# Set Zsh as default terminal
+sudo chsh -s $(which zsh)
+
+# NOTE: Install Warp
+sudo apt-get install -y wget gpg
+wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
+sudo install -y -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
+rm warpdotdev.gpg
+sudo apt update && sudo apt install -y warp-terminal
+
+# NOTE: Install Starship
+sudo curl -sS https://starship.rs/install.sh | sh
+
+# NOTE: Set Custom Prompt
+# Copy startship.toml to ~/.config/starship.toml
+starship preset pure-preset -o ~/.config/starship.toml
+
+# NOTE: Install NodeJS & NVM (Node Version Manager)
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# Download and install NodeJS
+sudo nvm install 22
